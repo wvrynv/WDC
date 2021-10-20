81b6f412eb1f4652e8d8272a6b9f2baa6158deb7
Maryna <62389144+wvrynv@users.noreply.github.com>
2021-10-20 21:39:25 +0300

    Update fetchCSV.js

--- docs/fetchCSV.js
+++ docs/fetchCSV.js
@@ -1,12 +1,12 @@
 $(document).ready(function (datasource) {
 
 	var indicator = $('#CSV').val();
-	var datasource = "https://api.us-west-2.parsable.net/api/analytics/extract/74ab4caf-652b-4fde-be1b-60aac817bc13.csv?start=1631577600&end=1631577730&type=inputs&tz=America%2NewYork&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzQwNzE3MjgsImlzcyI6ImF1dGg6cHJvZHVjdGlvbiIsInNlcmE6Y3J0ciI6IjdlM2Q5NWNiLWU1OTgtNDkwNC1hOTFiLTYzYmM1ZDZiYzdiNyIsInNlcmE6c2lkIjoiZDBiNTdjY2UtZmFlNi00M2UyLWFkYjctZTgyOTkzNmUyNTQwIiwic2VyYTp0ZWFtSWQiOiI3NGFiNGNhZi02NTJiLTRmZGUtYmUxYi02MGFhYzgxN2JjMTMiLCJzZXJhOnR5cCI6InBlcnNpc3RlbnQiLCJzdWIiOiIwMmE4MWQyZS1kN2Y0LTQyZjctOTI3NC1mOGEyNmYyZDJiZDcifQ.z1l-Kusksnia_T94fyXBFEEtaEVnHS9gRrVpeJVFl-o";
+	var datasource = "http://localhost:8889/" + indicator;
 	var myConnector = tableau.makeConnector();
 	
 	$('#CSV').on('change keyup paste click', function() {
     indicator = $('#CSV').val();
-	datasource = "https://api.us-west-2.parsable.net/api/analytics/extract/74ab4caf-652b-4fde-be1b-60aac817bc13.csv?start=1631577600&end=1631577730&type=inputs&tz=America%2NewYork&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzQwNzE3MjgsImlzcyI6ImF1dGg6cHJvZHVjdGlvbiIsInNlcmE6Y3J0ciI6IjdlM2Q5NWNiLWU1OTgtNDkwNC1hOTFiLTYzYmM1ZDZiYzdiNyIsInNlcmE6c2lkIjoiZDBiNTdjY2UtZmFlNi00M2UyLWFkYjctZTgyOTkzNmUyNTQwIiwic2VyYTp0ZWFtSWQiOiI3NGFiNGNhZi02NTJiLTRmZGUtYmUxYi02MGFhYzgxN2JjMTMiLCJzZXJhOnR5cCI6InBlcnNpc3RlbnQiLCJzdWIiOiIwMmE4MWQyZS1kN2Y0LTQyZjctOTI3NC1mOGEyNmYyZDJiZDcifQ.z1l-Kusksnia_T94fyXBFEEtaEVnHS9gRrVpeJVFl-o";
+	datasource = "http://localhost:8889/" + indicator;
 	tableau.connectionData = datasource;
 	});
 	
@@ -100,8 +100,7 @@ $(document).ready(function (datasource) {
 	$(document).ready(function () {
 		$("#submitButton").click(function () {
 			indicator = $('#CSV').val();
-			datasource = "https://api.us-west-2.parsable.net/api/analytics/extract/74ab4caf-652b-4fde-be1b-60aac817bc13.csv?start=1631577600&end=1631577730&type=inputs&tz=America%2NewYork&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzQwNzE3MjgsImlzcyI6ImF1dGg6cHJvZHVjdGlvbiIsInNlcmE6Y3J0ciI6IjdlM2Q5NWNiLWU1OTgtNDkwNC1hOTFiLTYzYmM1ZDZiYzdiNyIsInNlcmE6c2lkIjoiZDBiNTdjY2UtZmFlNi00M2UyLWFkYjctZTgyOTkzNmUyNTQwIiwic2VyYTp0ZWFtSWQiOiI3NGFiNGNhZi02NTJiLTRmZGUtYmUxYi02MGFhYzgxN2JjMTMiLCJzZXJhOnR5cCI6InBlcnNpc3RlbnQiLCJzdWIiOiIwMmE4MWQyZS1kN2Y0LTQyZjctOTI3NC1mOGEyNmYyZDJiZDcifQ.z1l-Kusksnia_T94fyXBFEEtaEVnHS9gRrVpeJVFl-o";
-
+			datasource = "http://localhost:8889/" + indicator;
 			tableau.connectionData = datasource;
 			tableau.connectionName = "WDCcsv";
 			tableau.connectionData = datasource;
